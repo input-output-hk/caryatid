55ad74cc5a5e85abd31e2c5bdb3eaaee95cfbbc4
Paul Clark <paul.clark@iohk.io>
2024-10-16 13:24:55 +0100

    Load modules from config

--- process/src/main.rs
+++ process/src/main.rs
@@ -2,13 +2,13 @@
 // Loads and runs modules built with caryatid-sdk
 
 use caryatid_sdk::*;
-use anyhow::Result;
+use anyhow::{Context as AnyhowContext, Result};
 use std::sync::Arc;
 use libloading::{Library, Symbol};
 use serde_json::json;
 use config::{Config, File, Environment};
 
-/// Helper function to extract a sub-config as a new Config object
+/// Extract a sub-config as a new Config object
 /// Defaults to an empty Config if the path does not exist.
 fn get_config(config: &Config, path: &str) -> Config {
     // Try to extract the sub-config as a table
@@ -26,6 +26,28 @@ fn get_config(config: &Config, path: &str) -> Config {
             Config::default()
         }
     }
+}
+
+/// Load and initialise a module
+fn load_module(lib_name: String, context: &Context) -> Result<()> {
+    let module_path = context.config.get_string("paths.modules")
+        .unwrap_or(".".to_string());
+    let module_file = module_path + "/" + &lib_name;
+    println!("Loading module from {}", module_file);
+
+    unsafe {
+        let module_lib = Library::new(module_file)?;
+        let module_creator: Symbol<unsafe extern "C" fn(&Context)
+                                                        -> *mut dyn Module> =
+            module_lib.get(b"create_module")?;
+
+        // Create the module
+        let module = module_creator(&context).as_ref()
+            .context("Can't create module")?;
+        println!("Created module {}", module.get_name());
+    }
+
+    Ok(())
 }
 
 /// Main process
@@ -47,20 +69,14 @@ async fn main() -> Result<()> {
     let context = Context::new(Arc::new(config),
                                message_bus.clone());
 
-    // Dynamically load the module
-    unsafe {
-        let module_lib = Library::new("./target/debug/libsample_module.so")
-            .expect("Failed to load module");
-        let module_creator: Symbol<unsafe extern "C" fn(&Context)
-                                                        -> *mut dyn Module> =
-            module_lib.get(b"create_module")
-            .expect("Failed to load create_module symbol");
 
-        // Create the module
-        let module = module_creator(&context).as_ref()
-            .expect("Failed to create module");
+    // Scan for modules
+    let modules_section = context.config.get_table("modules")?;
+    for (key, _value) in modules_section.into_iter() {
+        println!("Found module '{}'", key);
 
-        println!("Created module {}", module.get_name());
+        // Load the module
+        load_module(format!("lib{}_module.so", key), &context)?;
     }
 
     // Send a test JSON message to the message bus on 'sample_topic'
