a6181ce951511f174d229435e44dbbca85d9d380
Paul Clark <paul.clark@iohk.io>
2024-11-28 11:59:06 +0000

    Basic tests on InMemoryBus

--- process/src/in_memory_bus.rs
+++ process/src/in_memory_bus.rs
@@ -130,3 +130,94 @@ impl<M: MessageBounds> MessageBus<M> for InMemoryBus<M> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use config::{Config, FileFormat};
+    use tracing::Level;
+    use tracing_subscriber;
+    use tokio::sync::Notify;
+    use tokio::time::{timeout, Duration};
+    use futures::future::ready;
+
+    // Helper to set up an in-memory bus from given config string
+    struct TestSetup<M: MessageBounds> {
+        bus: Arc<dyn MessageBus<M>>
+    }
+
+    impl<M: MessageBounds> TestSetup<M> {
+
+        fn new(config_str: &str) -> Self {
+
+            // Set up tracing
+            let _ = tracing_subscriber::fmt()
+                .with_max_level(Level::DEBUG)
+                .with_test_writer()
+                .try_init();
+
+            // Parse config
+            let config = Config::builder()
+                .add_source(config::File::from_str(config_str, FileFormat::Toml))
+                .build()
+                .unwrap();
+
+            // Create the bus
+            let bus = Arc::new(InMemoryBus::<M>::new(&config));
+
+            Self { bus }
+        }
+    }
+
+    #[tokio::test]
+    async fn publish_subscribe_round_trip() {
+        let setup = TestSetup::<String>::new("");
+        let notify = Arc::new(Notify::new());
+
+        // Subscribe
+        let notify_clone = notify.clone();
+        assert!(setup.bus.register_subscriber(
+            "test",
+            Arc::new(move |_topic: &str, _message: Arc<String>| {
+                notify_clone.notify_one(); // Notify that the callback was triggered
+                Box::pin(ready(()))
+            }))
+                .await
+                .is_ok());
+
+        // Publish
+        assert!(setup.bus.publish("test", Arc::new("Hello, world!".to_string()))
+                .await
+                .is_ok());
+
+        // Wait for it to be received, or timeout
+        assert!(timeout(Duration::from_millis(100), notify.notified()).await.is_ok(),
+                "Didn't receive the subscribed message");
+    }
+
+    #[tokio::test]
+    async fn publish_subscribe_with_wrong_topic_doesnt_round_trip() {
+        let setup = TestSetup::<String>::new("");
+        let notify = Arc::new(Notify::new());
+
+        // Subscribe
+        let notify_clone = notify.clone();
+        assert!(setup.bus.register_subscriber(
+            "test",
+            Arc::new(move |_topic: &str, _message: Arc<String>| {
+                notify_clone.notify_one(); // Notify that the callback was triggered
+                Box::pin(ready(()))
+            }))
+                .await
+                .is_ok());
+
+        // Publish
+        assert!(setup.bus.publish("BOGUS", Arc::new("Hello, world!".to_string()))
+                .await
+                .is_ok());
+
+        // Wait for it to be received, or timeout
+        assert!(timeout(Duration::from_millis(100), notify.notified()).await.is_err(),
+                "Received the subscribed message when we shouldn't have!");
+    }
+
+}
