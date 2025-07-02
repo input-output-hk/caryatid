e4d668848a52684420508062133fb5282799e1b4
Alex Woods <alexander.j.woods@gmail.com>
2025-07-02 16:04:30 +0100

    Refactor match_topic to avoid heap allocation
    - also handle more cases of # usage

--- sdk/src/match_topic.rs
+++ sdk/src/match_topic.rs
@@ -1,40 +1,43 @@
 //! Helper function to pattern match topics
+use std::iter::Peekable;
+use std::str::Split;
 
 /// Match a dotted topic against a pattern - implements as RabbitMQ:
 ///   * - match one word
 ///   # - match zero or more words
 pub fn match_topic(pattern: &str, topic: &str) -> bool {
-    let pattern_parts: Vec<&str> = pattern.split('.').collect();
-    let topic_parts: Vec<&str> = topic.split('.').collect();
+    let pattern_split = pattern.split('.').peekable();
+    let topic_split = topic.split('.').peekable();
 
-    let mut i = 0;
-    let mut j = 0;
+    match_topic_split(pattern_split, topic_split)
+}
 
-    while i < pattern_parts.len() && j < topic_parts.len() {
-        match pattern_parts[i] {
+fn match_topic_split(mut pattern: Peekable<Split<char>>, mut topic: Peekable<Split<char>>) -> bool {
+    while let Some(pattern_part) = pattern.next() {
+        match pattern_part {
             // * matches exactly one word
             "*" => {
-                i += 1;
-                j += 1;
+                topic.next();
             }
 
             // # matches zero or more words
             "#" => {
-                if i == pattern_parts.len() - 1 {
+                // Eat any extra #
+                while pattern.peek() == Some("#").as_ref() {
+                    pattern.next();
+                }
+                if pattern.peek() == None {
                     // All the rest
                     return true;
                 }
 
                 // Try to match the next part of the pattern to any
                 // subsequent part of the topic
-                while j < topic_parts.len() {
-                    if match_topic(
-                        &pattern_parts[i + 1..].join("."),
-                        &topic_parts[j..].join("."),
-                    ) {
+                while let Some(_) = topic.peek() {
+                    if match_topic_split(pattern.clone(), topic.clone()) {
                         return true;
                     }
-                    j += 1;
+                    topic.next();
                 }
 
                 // No match found for the rest of the topic after #
@@ -42,9 +45,8 @@ pub fn match_topic(pattern: &str, topic: &str) -> bool {
             }
 
             // Direct match for a part?
-            part if part == topic_parts[j] => {
-                i += 1;
-                j += 1;
+            pattern_part if Some(pattern_part) == topic.peek().map(|v| &**v) => {
+                topic.next();
             }
 
             // No match
@@ -53,7 +55,7 @@ pub fn match_topic(pattern: &str, topic: &str) -> bool {
     }
 
     // If we reached the end of both the pattern and topic, it's a match
-    i == pattern_parts.len() && j == topic_parts.len()
+    topic.peek() == None
 }
 
 // -- Tests --
@@ -81,8 +83,16 @@ mod tests {
         assert!(match_topic("#", "foo"));
         assert!(match_topic("#", "foo.bar.baz"));
         assert!(match_topic("foo.#", "foo.bar.baz"));
+        assert!(match_topic("foo.#.#", "foo.bar.baz"));
+        assert!(match_topic("foo.#", "foo"));
+        assert!(match_topic("foo.#.#", "foo"));
         assert!(match_topic("foo.bar.#", "foo.bar.baz"));
         assert!(match_topic("foo.#.baz", "foo.bar.baz"));
+        assert!(match_topic("foo.#.baz", "foo.baz"));
+        assert!(match_topic("foo.#.#.baz", "foo.baz"));
+        assert!(match_topic("foo.#.baz", "foo.bar.something.more.baz"));
+        assert!(match_topic("#.baz", "baz"));
+        assert!(match_topic("#.#.baz", "baz"));
         assert!(match_topic("#.baz", "foo.bar.baz"));
         assert!(!match_topic("#.splat", "foo.bar.baz"));
         assert!(!match_topic("foo.bar.baz#", "foo.bar.baz"));
@@ -94,5 +104,9 @@ mod tests {
         assert!(match_topic("foo.#", "foo.bar.baz"));
         assert!(!match_topic("foo.*", "foo.bar.baz"));
         assert!(match_topic("foo.*.#", "foo.bar.baz"));
+        assert!(match_topic("foo.*.#.baz", "foo.bar.baz"));
+        assert!(match_topic("foo.*.#.*", "foo.bar.baz"));
+        assert!(match_topic("foo.#.bar.#.*", "foo.bar.baz"));
+        assert!(!match_topic("foo.*.#.*.baz", "foo.bar.baz"));
     }
 }
